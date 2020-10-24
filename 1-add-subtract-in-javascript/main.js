bbea2f32371058ba255fb1de3b47561d38599e61
Tu Phan <tuantu.phan@cimb.com>
2020-10-24 21:14:41 +0800

    Add Substract in Javascript

---
+++ 1-add-subtract-in-javascript/main.js
@@ -0,0 +1,31 @@
+
+// ------------
+// Add
+// ------------
+
+// Numer + Number = Number
+console.log(1 + 1);
+
+// String  + String = String
+console.log('Easy' + 'Frontend');
+
+
+// Number + String = String + String = String
+
+console.log(100 + '200');
+console.log(100 + 'subcribers');
+console.log(100 + 100 + '200');
+
+// Number - Number = Number
+console.log(2 - 1);
+
+// String - String = NaN
+console.log('abc' - 'def');
+console.log(100 - 'abc');
+console.log('abc' - 100);
+
+console.log(1000 - '500' - 200);
+
+//console.log(100 + 100 + '100' - 100);
+console.log(100 + '100' - '100' - '100');
+//console.log(100 + (100 + '200') - '300');
